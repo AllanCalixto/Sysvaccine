ea76ec01d392fc38bffe57bba4424c79e7bd515e
felipesalles <felipemdesalles@gmail.com>
2019-11-06 19:58:18 -0300

    cirado arquivo css para a view emitir cartao, feito reset da margin e do padding no css relacioando a view  adicionado o favicon na mesma view

--- resources/views/emitircartao.blade.php
+++ resources/views/emitircartao.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/f82b3ec5fe.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}" type="image/x-png" />
+    <link rel="stylesheet" href="{{asset('css/emitircartao.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Document</title>
